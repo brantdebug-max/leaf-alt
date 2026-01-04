e926a56003a3545c2910130d5c9acdc57d1dc35b
brantdebug-max <brantdebug@gmail.com>
2026-01-04 06:39:37 +0100

    Create Legal.php

---
+++ leaf/Legal.php
@@ -0,0 +1,11 @@
+<?php include "../includes/header.php"; ?>
+
+<section class="container page">
+  <h1>Legal</h1>
+  <p>
+    All property listings and services are provided in compliance
+    with applicable laws and regulations.
+  </p>
+</section>
+
+<?php include "../includes/footer.php"; ?>
