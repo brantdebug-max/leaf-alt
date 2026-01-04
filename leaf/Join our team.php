addebc576536a9204d7a6e6ec7fdcbe36f5aec67
brantdebug-max <brantdebug@gmail.com>
2026-01-04 06:38:27 +0100

    Create Join our team.php

---
+++ leaf/Join our team.php
@@ -0,0 +1,15 @@
+<?php include "../includes/header.php"; ?>
+
+<section class="container page">
+  <h1>Join Our Team</h1>
+  <p>We’re always looking for talented professionals.</p>
+
+  <form>
+    <input type="text" placeholder="Full Name" required><br><br>
+    <input type="email" placeholder="Email Address" required><br><br>
+    <textarea placeholder="Why do you want to join us?"></textarea><br><br>
+    <button class="btn-primary">Submit Application</button>
+  </form>
+</section>
+
+<?php include "../includes/footer.php"; ?>
