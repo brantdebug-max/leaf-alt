ce0fd668bd127e52210e42fc5a1aec215b4b93e1
brantdebug-max <brantdebug@gmail.com>
2026-01-04 06:40:54 +0100

    Create Support.php

---
+++ leaf/Support.php
@@ -0,0 +1,10 @@
+<?php include "../includes/header.php"; ?>
+
+<section class="container page">
+  <h1>Support</h1>
+  <p>
+    Need help? Contact our support team and we’ll assist you promptly.
+  </p>
+</section>
+
+<?php include "../includes/footer.php"; ?>
